3fb0780a4b303ea96ca55bbc31351b0c50f87d1d
Rajesh Subramanian <rsubramanian@resourceammirati.com>
2018-10-29 23:02:09 -0400

    Added comments

--- src/com/rsubramanian/java101/HelloWorld.java
+++ src/com/rsubramanian/java101/HelloWorld.java
@@ -1,8 +1,13 @@
+// Package name
 package com.rsubramanian.java101;
 
+// Class declaration
 public class HelloWorld {
 
+	// Main method
 	public static void main(String[] args) {
+
+		// Print out something
 		System.out.println("Hello World");
 	}
 }
